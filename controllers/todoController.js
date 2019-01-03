b1d53557fbb1b83f628c044bf5b399459849ab37
Shubham Rana <rana11shubham@gmail.com>
2019-01-03 18:23:13 +0530

    added database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,27 +1,65 @@
 var bodyParser=require('body-parser');
 var urlencodedParser=bodyParser.urlencoded({extended:false});
+var mongoose=require('mongoose');
+mongoose.Promise= global.Promise;
+//To connect local mongodb
+mongoose.connect('mongodb://localhost:27017/todo',{ useNewUrlParser: true });
+//To create Schema
+var TodoSchema=new mongoose.Schema({
+    item:String
+});
+//To model the databasee
+var Todo=mongoose.model('Todo',TodoSchema);
 var arr=['Walking','Swimming'];
 module.exports = function(app){
 
+app.get('/',(req,res)=>{
+  res.redirect('/todo');
+})
 app.get('/todo',(req,res)=>{
-    res.render('todo',{todo:arr});
+  Todo.find().then((data)=>{
+    res.render('todo',{todo:data});
+  },(err)=>{
+    throw err;
+  });
 });
+
 
 app.post('/todo',urlencodedParser,(req,res)=>{
 //  var json=JSON.stringify(req.body.todo);
   //console.log(req.body.todo);
-  if(req.body.todo.length!==0){
-  arr.push(req.body.todo);
-  res.json(arr);
+   if(req.body.todo.length!==0){
+     var todo=req.body.todo;
+  var newTodo=new Todo({item:todo}).save(function(err,data){
+    if(err) throw err;
+    res.json(newTodo);
+  });
 }
 else{
   return;
 }
+//   var todo=Todo()
+//   //arr.push(req.body.todo);
+//   res.json(arr);
+// }
+// else{
+//   return;
+// }
 });
 
 app.delete('/todo/:item',(req,res)=>{
-  arr=arr.filter(item=>item!==req.params.item);
-     console.log(arr);
-     res.json(arr);
-});
+  //arr=arr.filter(item=>item!==req.params.item);
+    // console.log(arr);
+    Todo.findOne({item:req.params.item}).then((data)=>{
+     Todo.deleteMany({item:data.item},(err)=>{
+         if(err) return handleError(err);
+         //console.log(data.item);
+         res.json(data.item);
+       });
+     },(err)=>{
+       if(err) throw err;
+     });
+      // res.json(data);
+    });
+//});
 }
